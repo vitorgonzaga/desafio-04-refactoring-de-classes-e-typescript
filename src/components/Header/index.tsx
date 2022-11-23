e7b399bd1b14dad3503ced9263ea229f5d15de3a
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-11-23 00:08:12 -0300

    refactor Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
-import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
+import { Container } from './styles';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void
+}
 
+export default function Header({ openModal }: HeaderProps): JSX.Element {
     return (
       <Container>
         <header>
@@ -28,7 +28,4 @@ class Header extends Component {
         </header>
       </Container>
     )
-  }
 };
-
-export default Header;
