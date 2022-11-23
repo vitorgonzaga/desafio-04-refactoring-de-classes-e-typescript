909438123cfcad54a1afd78bef1167bf93471549
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-11-23 00:45:42 -0300

    refactoring Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,28 +1,22 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean,
+  setIsOpen: () => void,
+  children: JSX.Element
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+export default function Modal({
+  isOpen,
+  setIsOpen,
+  children,
+}: ModalProps): JSX.Element {
+    const [ modalStatus, setModalStatus ] = useState(isOpen)
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+  useEffect(() => {
+    setModalStatus(isOpen)
+  }, [isOpen])
 
     return (
       <ReactModal
@@ -52,7 +46,4 @@ class Modal extends Component {
         {children}
       </ReactModal>
     );
-  }
 };
-
-export default Modal;
